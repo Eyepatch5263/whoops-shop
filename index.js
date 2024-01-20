4f0ac22bf977aea52a0f0a6bcd69abdb91218adf
Eyepatch <pratyushpragyey@gmail.com>
2024-01-21 02:53:23 +0530

    step-2

---
+++ index.js
@@ -0,0 +1,11 @@
+const showSidebar=()=>{
+    const sidebar=document.querySelector(".sidebar")
+    sidebar.style.display="flex"
+    console.log("test")
+    
+}
+
+const vanish=()=>{
+    const sidebar=document.querySelector(".sidebar")
+    sidebar.style.display="none"
+}
